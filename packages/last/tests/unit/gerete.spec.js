f02f2be32481c14c06ddc3eed3902d7bac21c66f
Marcon Willian <marconwillian@protonmail.com>
2020-03-27 16:35:44 -0300

    1.0.21 - Config test for test

--- packages/last/tests/unit/gerete.spec.js
+++ packages/last/tests/unit/gerete.spec.js
@@ -3,6 +3,6 @@ const gerate = require('../../src/gerate');
 describe('Config test for test', () => {
     it('Sum 2 number.', () => {
         const config = gerate(4, 8);
-        expect(config).toBe(11);
+        expect(config).toBe(12);
     })
 })
