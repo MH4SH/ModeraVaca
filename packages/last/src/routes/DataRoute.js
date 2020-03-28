c7ed2f5a46435c4429972675cbdef7cf97bc2483
Marcon Willian <marconwillian@protonmail.com>
2020-03-28 15:00:27 -0300

    1.0.38 - Create Validator for "DATA"

--- packages/last/src/routes/DataRoute.js
+++ packages/last/src/routes/DataRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const DataRoute = express.Router();
 
-const DataController = require('./../Controllers/DataController');
+const DataController = require('../controllers/DataController');
+const DataValidator = require('../validator/DataValidator');
 
-DataRoute.post('/data', DataController.create);
+DataRoute.post('/data', DataValidator.create, DataController.create);
 DataRoute.get('/data', DataController.index);
-DataRoute.patch('/data/:_id', DataController.update);
-DataRoute.patch('/data/:_id/status', DataController.updateStatus);
+DataRoute.patch('/data/:_id', DataController.update, DataController.update);
+DataRoute.patch('/data/:_id/status', DataController.updateStatus, DataController.updateStatus);
 
 module.exports = DataRoute;
