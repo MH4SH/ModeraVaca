141ed4e8ac17aa661e1ac9b807b63ad68b86aee5
Marcon Willian <marconwillian@protonmail.com>
2020-03-30 22:50:59 -0300

    backend  -1.1.14 > create a validator with need of resonse for transaction

--- packages/last/backend/src/controllers/TransactionController.js
+++ packages/last/backend/src/controllers/TransactionController.js
@@ -9,10 +9,13 @@ module.exports = {
             perPage = 5;
 
         const count = await TransactionSB.countDocuments({_user});
-        const transactions = await TransactionSB.find({_user}, {__v: false, _user: false}).sort({date: -1}).skip((page-1)*perPage).limit(perPage);
-
         res.header('X-Total-Count', count);
-        res.header('X-Per-Page', perPage);
+        res.header('X-Per-Page', perPage)
+
+        if(count===0)
+            return res.status(204).json([]);
+            
+        const transactions = await TransactionSB.find({_user}, {__v: false, _user: false}).sort({date: -1}).skip((page-1)*perPage).limit(perPage);
 
         res.status(200).json({_user, transactions});
     }
