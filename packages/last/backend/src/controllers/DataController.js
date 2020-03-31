4d511a301b50a3e36aa01974519165aa7096761b
Marcon Willian <marconwillian@protonmail.com>
2020-03-31 08:38:38 -0300

    backend - 1.1.20 > add auth on list data

--- packages/last/backend/src/controllers/DataController.js
+++ packages/last/backend/src/controllers/DataController.js
@@ -4,7 +4,8 @@ const Datas = mongoose.model('Datas');
 
 module.exports = {
     index: async (req, res) => {
-        const datas = await Datas.find().sort({number: 1});
+        const _user = req._user;
+        const datas = await Datas.find({_user}).sort({number: 1});
         var response = {salesman: [], breed: [], buyer: []};
         datas.map(({_id, value, number, kind}) => { response[kind].push({_id, value, number})});
 
