d113592e5c3b39b74e2040e5cd7492d963952d27
Marcon Willian <marconwillian@protonmail.com>
2020-03-31 08:18:47 -0300

    backend - 1.1.17 > add auth on sale list

--- packages/last/backend/src/controllers/SaleController.js
+++ packages/last/backend/src/controllers/SaleController.js
@@ -6,25 +6,21 @@ const SaleDB = mongoose.model('Sales');
 module.exports = {
     index: async (req, res) => {
         const {page = 1} = req.query,
+            _user = req._user,
             perPage = 10;
 
-        const filter = {kind: "sale"};
+        const filter = {kind: "sale", _user};
         
         const count = await SaleDB.countDocuments(filter);
-        const transaction = await SaleDB.find(filter).skip((page - 1)*perPage).limit(perPage);
-
-        const response = [];
-
-        transaction.map(({_id, buyer, purchase, date, amount, cattle}) => response.push({_id, buyer, purchase, date, amount, cattle}));
-        
         res.header('X-Total-Count', count);
         res.header('X-Per-Page', perPage)
 
-        if(!transaction[0]){
+        if(count===0)
             return res.status(204).json([]);
-        }
+            
+        const transaction = await SaleDB.find(filter).skip((page - 1)*perPage).limit(perPage).select({__v: false, kind: false, _user: false});        
 
-        res.status(200).json(response);
+        res.status(200).json(transaction);
     },
     create: async (req, res) => {
         const {buyer, purchase, date, amount, cattle} = req.body;
