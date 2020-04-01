13fdba62bc3d18a0e592363344f8f311795d471c
Marcon Willian <marconwillian@protonmail.com>
2020-04-01 15:41:01 -0300

    fontend - 0.1.4 > update react

---
+++ packages/last/frontend/src/pages/Logon/index.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import './styles.css';
+
+export default function Logon() {
+    const [id, setId] = useState("");
+
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try{
+            const response = await api.post('session', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name)
+            
+            history.push('/perfil')
+        } catch(err){
+            alert('Falha no login, tente novamente.');
+        }
+    }
+    return (
+        <div className="logon-container">
+            <section className="form">
+
+                <form onSubmit={handleLogin}>
+                    <h1>Faça seu logon</h1>
+
+                    <input 
+                        placeholder="Seu ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}    
+                    />
+                    <button className="button" type="submit">Entrar</button>
+
+
+                    <Link to="/registrar" className="back-link">
+                        <FiLogIn size={16} color="#e02041"/>
+                        Não tenho cadastro
+                    </Link>
+                </form>
+            </section>
+
+        </div>
+    )
+}
