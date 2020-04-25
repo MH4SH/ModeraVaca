42373fc2b269be402f899716adf0304b8f345ba5
Marcon Willian <marconwillian@protonmail.com>
2020-04-25 16:05:15 -0400

    backend - 2.0.2 > Finifhs Change | finish #29

--- packages/last/backend/src/routes/index.js
+++ packages/last/backend/src/routes/index.js
@@ -2,10 +2,9 @@ const {errors} = require('celebrate')
 
 const authMiddlewares = require('../middlewares/auth');
 
+const CardRoute = require('./CardRoute');
 const TransactionRoute = require('./TransactionRoute');
 const DataRoute = require('./DataRoute');
-const PurchaseRoute = require('./PurchaseRoute');
-const SaleRoute = require('./SaleRoute');
 const UserRoute = require('./UserRoute');
 
 
@@ -16,8 +15,7 @@ module.exports = app => {
 
     app.use('/transaction', authMiddlewares, TransactionRoute);
     app.use('/data', authMiddlewares, DataRoute);
-    app.use('/purchase', authMiddlewares, PurchaseRoute);
-    app.use('/sale', authMiddlewares, SaleRoute);
+    app.use('/card', authMiddlewares, CardRoute);
     app.use(errors());
     app.use((req, res) => {
         res.status(404).json({error: "Sorry can't find that!"})
