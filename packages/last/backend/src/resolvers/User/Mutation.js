1a889f5243fdf18e808aa92137a17ae309b5f075
Marcon Willian <marconwillian@protonmail.com>
2020-05-07 19:32:33 -0400

    backend - 3.0.4 > Add resolvers of User

--- packages/last/backend/src/resolvers/User/Mutation.js
+++ packages/last/backend/src/resolvers/User/Mutation.js
@@ -13,6 +13,26 @@ const createUser = async (_, args) => {
   }
 };
 
+const deleteUser = async (_, args) => {
+  try {
+    const data = usersList.find(user => user.id == 1);
+    return false;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
+
+const updateUser = async (_, args) => {
+  try {
+    const data = usersList.find(user => user.id == 1);
+    return data;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
+
 module.exports = {
     createUser,
+    deleteUser,
+    updateUser
 };
