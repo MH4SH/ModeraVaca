5783a1c9359b68cafd17bac6815730fe85947ef7
Marcon Willian <marconwillian@protonmail.com>
2020-06-19 14:16:55 -0300

    FInish mutation of sale

--- packages/server/src/resolvers/Sale/Mutation.js
+++ packages/server/src/resolvers/Sale/Mutation.js
@@ -1,18 +1,5 @@
 const connection = require('../../database/connection');
 
-const saleList = [
-  {id: 1, gender: 'f', age: 1, idCard: 1, amount: 12},
-  {id: 2, gender: 'f', age: 2, idCard: 1, amount: 22},
-  {id: 3, gender: 'f', age: 3, idCard: 1, amount: 11},
-  {id: 4, gender: 'f', age: 4, idCard: 1, amount: 14},
-  {id: 5, gender: 'f', age: 2, idCard: 2, amount: 2},
-  {id: 6, gender: 'f', age: 1, idCard: 2, amount: 22},
-  {id: 7, gender: 'f', age: 3, idCard: 2, amount: 23},
-  {id: 8, gender: 'f', age: 4, idCard: 2, amount: 44},
-  {id: 9, gender: 'f', age: 6, idCard: 2, amount: 11},
-  {id: 10, gender: 'm', age: 7, idCard: 1, amount: 1}
-];
-
 const createSale = async (_, args) => {
   try {
     const {idBreeds, gender, AgeGroup, amount} = args.input,
@@ -71,8 +58,31 @@ const createSale = async (_, args) => {
 
 const deleteSale = async (_, args) => {
   try {
-    const data = saleList.find(Sale => Sale.id == 1);
-    return false;
+    let listAnimals = await connection('transaction_with_animal')
+      .where('idTransaction', args.id)
+      .where('type', 'sale');
+
+    const trx = await connection.transaction();
+
+    await trx('sale')
+      .where('id', args.id)
+      .delete();
+
+    for(let {idAnimal} of listAnimals){
+      await trx('animal')
+        .where('id', idAnimal)
+        .update({ hasNow: true });
+
+      await trx('transaction_with_animal')
+        .where('idAnimal', idAnimal)
+        .where('idTransaction', args.id)
+        .where('type', 'sale')
+        .delete();
+    }
+
+    await trx.commit();
+
+    return true;
   } catch (e) {
     throw new Error(e.message);
   }
