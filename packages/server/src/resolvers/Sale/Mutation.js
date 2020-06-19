610579bb8d390af26456eb24af8e9b2f6aa439ad
Marcon Willian <marconwillian@protonmail.com>
2020-06-19 08:39:50 -0300

    Start work on Sale

--- packages/server/src/resolvers/Sale/Mutation.js
+++ packages/server/src/resolvers/Sale/Mutation.js
@@ -1,3 +1,5 @@
+const connection = require('../../database/connection');
+
 const saleList = [
   {id: 1, gender: 'f', age: 1, idCard: 1, amount: 12},
   {id: 2, gender: 'f', age: 2, idCard: 1, amount: 22},
@@ -13,8 +15,55 @@ const saleList = [
 
 const createSale = async (_, args) => {
   try {
-    const data = saleList.find(Sale => Sale.id == 1);
-    return data;
+    const {idBreeds, gender, AgeGroup, amount} = args.input,
+      data = args.input;
+
+    let age = AgeGroup(new Date());
+
+    let listAnimals = await connection('animal')
+      .select('id')
+      .where('idBreeds', idBreeds)
+      .where('gender', gender)
+      .where('hasNow', true)
+      .where('dateBorn', '<=', age.start.getTime())
+      .where('dateBorn', '>', age.end.getTime());
+
+    if(listAnimals.length<amount)
+      throw new Error(JSON.stringify({status: "Don't have animal"}));
+
+    
+    delete data.AgeGroup;
+
+    const trx = await connection.transaction();
+
+    const [saleId] = await trx('dead').insert({
+      ...data,
+      idFarm: args.idFarm,
+      created: new Date()
+    });
+
+
+    for(let v = 0; v < amount; v++){
+      let idAnimal = listAnimals[v];
+      await trx('animal')
+        .where('id', idAnimal)
+        .update({
+          hasNow: false
+        });
+
+      await trx('transaction_with_animal').insert({
+        idAnimal,
+        idTransaction: saleId,
+        type: 'sale'
+      });
+    }
+
+    await trx.commit();
+
+    return {
+        id: saleId,
+        ...data
+      };
   } catch (e) {
     throw new Error(e.message);
   }
