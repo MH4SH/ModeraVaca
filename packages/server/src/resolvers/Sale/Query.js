fc8465bf8c7ec59ac08afb22301062939d4572e9
Marcon Willian <marconwillian@protonmail.com>
2020-07-19 09:51:04 -0300

    3.15.7 - Create pagination of sale

--- packages/server/src/resolvers/Sale/Query.js
+++ packages/server/src/resolvers/Sale/Query.js
@@ -1,26 +1,46 @@
 const connection = require('../../database/connection');
+
 const { authorizationUserHasFarm } = require('../../auth/utils/verifyUserAuthenticate');
-
-const pageInfo = {
-    endCursor: "CURSOR NÃO ARRUMADO",
-    hasNextPage: true
-}
-
+const { cursorEncoding, cursorDecoding } = require('../../utils/cursorEncodingAndDecoding');
 
 const sales = async (_, args, context) => {
     try {
         authorizationUserHasFarm(context);
 
-        const idFarm = context._userAuthenticate.idFarm;
+        const idFarm = context._userAuthenticate.idFarm,
+            filter = args.filter ? {...args.filter} : {},
+            limitPage = args.limit || 10,
+            cursor = cursorDecoding(args.cursor);
 
-        const current = "CURSOR NÃO ARRUMADO";
-        
+        let pageInfo = {
+            endCursor: null,
+            hasNextPage: false
+        }
+
+        const { totalCount } = await connection('sale')
+            .count({totalCount: '*'})
+            .where({idFarm, ...filter})
+            .first();
+
         const saleList = await connection('sale')
-            .where({idFarm});
+            .where('id', '>', cursor)
+            .where({idFarm, ...filter})
+            .limit(limitPage + 1);
+            
+        let amountItens = saleList.length;
+        pageInfo.hasNextPage = amountItens > limitPage;
+
+        const edges = saleList.slice(0, limitPage).map(item => {
+            let cursor = cursorEncoding(item.id);
+            pageInfo.endCursor = cursor;
+
+            return { node: item, cursor }
+        });
 
         return {
             pageInfo,
-            edges: saleList.map(item => ({ node: item, cursor: current })),
+            totalCount,
+            edges
         };
     } catch (e) {
         throw new Error(e.message);
