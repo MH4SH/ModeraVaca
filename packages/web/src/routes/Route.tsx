6b3bce2b28c369beb96c2de424e9e6d98674d7f4
MArcon Willian <marconwillian@protonmail.com>
2020-10-06 07:29:46 -0300

    fixUseRouteComponent

--- packages/web/src/routes/Route.tsx
+++ packages/web/src/routes/Route.tsx
@@ -29,7 +29,7 @@ const Route: React.FC<RouteProps> = ({
         ) : (
           <Redirect
             to={{
-              pathname: isPrivate ? "/login" : "/",
+              pathname: isPrivate ? "/entrar" : "/nascimentos",
               state: { from: location },
             }}
           />
